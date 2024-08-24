8d9be3633ef60d9e1c2f19660eb24c8bb0340ff8
shraddhha <125660723+shraddhha@users.noreply.github.com>
2024-08-24 07:00:12 +0530

    Create 0 - 1 Knapsack Problem.java

---
+++ 0 - 1 Knapsack Problem.java
@@ -0,0 +1,18 @@
+ int N = val.length;
+        int[][] dp = new int[N + 1][W + 1];
+
+        for (int i = 0; i <= N; i++) {
+            for (int w = 0; w <= W; w++) {
+                if (i == 0 || w == 0) {
+                    dp[i][w] = 0;
+                } else if (wt[i - 1] <= w) {
+                    dp[i][w] = Math.max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w]);
+                } else {
+                    dp[i][w] = dp[i - 1][w];
+                }
+            }
+        }
+
+        return dp[N][W];
+    }
+}
