ef61dcfbc607d6229ac2ed994c78598991307fe7
shraddhha <125660723+shraddhha@users.noreply.github.com>
2024-10-03 14:59:09 +0530

    Create Majority Vote.java

---
+++ Majority Vote.java
@@ -0,0 +1,57 @@
+class Solution {
+    // Function to find the majority elements in the array
+    public List<Integer> findMajority(List<Integer> nums) {
+        // Your code goes here.
+        int n = nums.size();
+        if (n == 0) {
+            return new ArrayList<>();
+        }
+
+        // Step 1: Find two potential candidates
+        int candidate1 = -1, candidate2 = -1, count1 = 0, count2 = 0;
+        
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+    
+        count1 = 0;
+        count2 = 0;
+        
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        if (count1 > n / 3) {
+            result.add(candidate1);
+        }
+        if (count2 > n / 3) {
+            result.add(candidate2);
+        }
+
+    
+        if (result.isEmpty()) {
+            result.add(-1);
+        }
+        return result;
+    
+    }
+}
